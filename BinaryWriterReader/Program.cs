8ace3b6eecdeb8b025343b46ffb4cf566f065d46
Servidor de Teste - BCSM <serverteste@bcsm.com.br>
2026-05-08 16:16:41 -0300

    att

---
+++ BinaryWriterReader/Program.cs
@@ -0,0 +1,21 @@
+﻿FileInfo file = new FileInfo("binFile.dat");
+
+using(BinaryWriter bw = new BinaryWriter(file.OpenWrite()))
+{
+    Console.WriteLine("Content {0}", bw.BaseStream);
+
+    var aDouble = 2.5;
+    var aInt = 5;
+    var aString = "abc";
+
+    bw.Write(aDouble);
+    bw.Write(aInt);
+    bw.Write(aString);
+}
+
+using(BinaryReader br = new BinaryReader(file.OpenRead()))
+{
+    Console.WriteLine(br.ReadDouble());
+    Console.WriteLine(br.ReadInt32());
+    Console.WriteLine(br.ReadString());
+}
---
+++ MyDirectoryWatcher/Program.cs
@@ -0,0 +1,37 @@
+﻿FileSystemWatcher watcher = new FileSystemWatcher();
+
+try
+{
+    watcher.Path = @".";
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex);
+    return;
+}
+
+watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+watcher.Filter = "*.txt";
+
+watcher.Changed += (s, e) => Console.WriteLine($"File: {e.FullPath} {e.ChangeType}!");
+watcher.Created += (s, e) => Console.WriteLine($"File: {e.FullPath} {e.ChangeType}!");
+watcher.Deleted += (s, e) => Console.WriteLine($"File: {e.FullPath} {e.ChangeType}!");
+
+watcher.Renamed += (s, e) => Console.WriteLine($"File: {e.OldFullPath} renamed to {e.FullPath}");
+
+watcher.EnableRaisingEvents = true;
+
+Console.WriteLine(@"Press 'q' to quit app.");
+
+using(var sw = File.CreateText("Test.txt"))
+{
+    await Task.Delay(3000);
+    sw.Write("This is some text");
+}
+await Task.Delay(3000);
+File.Move("Test.txt", "Test2.txt");
+await Task.Delay(3000);
+File.Delete("Test2.txt");
+
+while(Console.Read() != 'q');
